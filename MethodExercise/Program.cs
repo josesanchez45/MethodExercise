d57ec8fe8a8b3ac62aa60badcf4e6bdfcb006b07
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-05 15:14:38 -0700

    initial commit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercise 1
+            Console.WriteLine("What is your name?");
+            string name= Console.ReadLine();
+            Console.WriteLine($"Hello, {name}.");
+            Console.WriteLine("What is your favorite color?");
+            string color= Console.ReadLine();
+            Console.WriteLine($"Got it! Your favorite color is {color}!");
+            Console.WriteLine("What is your favorite animal?");
+            string animal= Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string band= Console.ReadLine();
+            Console.WriteLine($"What if {band} was actually a bunch of {animal}'s playing music? Funny!");
+
+        }
+    }
+}
