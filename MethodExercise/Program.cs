01bc19bc8c4f6a3431593936004bd519e37ad7c0
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-05 16:01:34 -0700

    added mod

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -8,13 +8,15 @@ namespace MethodExercise
         static double Multiply(int num1, int num2) { return num1 * num2; }
         static int Divide(int num1, int num2) { return num1 / num2; }
         static int Subtract(int num1, int num2) { return num1 - num2; }
+        static int Mod(int num1, int num2) { return num1 % num2; }
         static void Main(string[] args)
         {
            Console.WriteLine($"Lucky number is always { AddSum(3, 4)}");
            Console.WriteLine($"If you multiply you lucky number which is {AddSum(3, 4)} by itself you get {Multiply(7, 7)}");
             Console.WriteLine($"Lets go back to our lucky number {Divide(49,7)}");
             Console.WriteLine($"Well any number is ok even {Subtract(7, 7)}, they are all important.");
-
+            Console.WriteLine($"7 goes into 49 evenly, when you divide it the remainder is {Mod(49,7)}");
+            Console.WriteLine(" Enough about the number 7, lets learn about you!");
             //exercise 1
             Console.WriteLine("What is your name?");
             string name= Console.ReadLine();
